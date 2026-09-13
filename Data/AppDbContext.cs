ad7ea2dbd953f72504ca62f0acbac97bfcc8264e
jennykhranovska <jenny.khranovska@gmail.com>
2026-09-13 20:42:42 +0200

    Add AppDbContext with Recipe DbSet for EF Core
    Introduced AppDbContext in receptappen_api.Data, inheriting from DbContext. Added DbSet<Recipe> for managing Recipe entities and included necessary using directives.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using receptappen_api.Models;
+
+namespace receptappen_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
