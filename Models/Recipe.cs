1444e3f57f139d49511eaddbf6fc707005eb2ea3
jennykhranovska <jenny.khranovska@gmail.com>
2026-09-13 20:10:46 +0200

    Initial ASP.NET Core Web API project setup
    Added receptappen-api targeting .NET 10.0 with .gitignore, appsettings, and launchSettings. Configured Program.cs for controllers and Swagger. Added WeatherForecast sample, Recipe model, and HTTP request sample. Included EF Core Sqlite and OpenAPI NuGet packages. Added solution and user settings files.

---
+++ Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿namespace receptappen_api.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public string Category { get; set; } = "";
+
+        public int CookingTime { get; set; }
+    }
+}
